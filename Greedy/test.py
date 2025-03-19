6dbb920f8d13787c08ad2ad4e3d477846af98b15
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2025-03-19 21:05:39 +0800

    19/3/2025

--- Greedy/test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from Greedy376 import Solution
-
-class TestSolution(unittest.TestCase):
-    def test_wiggleMaxLength(self):
-        self.assertEqual(Solution().wiggleMaxLength([1,7,4,9,2,5]),6)
-        self.assertEqual(Solution().wiggleMaxLength([1,17,5,10,13,15,10,5,16,8]),7)
-        self.assertEqual(Solution().wiggleMaxLength([1,2,3,4,5,6,7,8,9]),2)
